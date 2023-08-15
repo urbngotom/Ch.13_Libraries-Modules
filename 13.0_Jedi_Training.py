d4a607f54258fb19ccbfbba304ced3c178648930
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-15 18:58:49 -0500

    Update 13.0_Jedi_Training.py

--- 13.0_Jedi_Training.py
+++ 13.0_Jedi_Training.py
@@ -1,8 +1,10 @@
 '''
-Sign your name:________________
- 
+# 13.0 Jedi Training (10pts)  Name:________________
+
+ BOX CLASS LIBRARY  (5pts)
+ -----------------
 For this test, take your 30 box program and remove the Box Class into a seperate file called Box_Builder.py. Now just
-import the Box Class into your main program. No need to pull request this. Just show it to your instructor if you can get
+import the Box Class into your main program. No need to pull submit this. Just show it to your instructor if you can get
 it to work. Use if __name__ =="__main__":
 
 '''
