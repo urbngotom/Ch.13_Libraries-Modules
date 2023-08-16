1b3e5162cb4beda28990aa9f2ed95050abbe1114
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-15 19:00:51 -0500

    Update 13.0_Jedi_Training.py

--- 13.0_Jedi_Training.py
+++ 13.0_Jedi_Training.py
@@ -4,7 +4,20 @@
  BOX CLASS LIBRARY  (5pts)
  -----------------
 For this test, take your 30 box program and remove the Box Class into a seperate file called Box_Builder.py. Now just
-import the Box Class into your main program. No need to pull submit this. Just show it to your instructor if you can get
-it to work. Use if __name__ =="__main__":
+import the Box Class into your main program. Use if __name__ =="__main__":
 
+'''
+
+
+
+'''
+FUNCTIONS LIBRARY  (5pts)
+ -----------------
+Paste all the functions that you submitted in the Functions chapter into a single file called my_library.py.
+This should only include all of the (defs), not the inputs and function calls. 
+Create a main program called my_program.py which will import the my_library module. 
+In this program you will put the inputs and function calls. 
+Use the import * so you don't have to use namespaces for each function call. 
+Use if __name__ =="__main__":
+You can just demonstrate this to your instructor when you have completed both of these tasks.
 '''
